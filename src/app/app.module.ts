8b592e27c590d85a751a2ea4904b5b80da87800e
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-05-17 22:09:21 +0300

    Create the project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { LogoComponent } from './logo/logo.component';
+import { LoadComponent } from './load/load.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    FooterComponent,
+    SearchBarComponent,
+    CourseCardComponent,
+    LogoComponent,
+    LoadComponent
   ],
   imports: [
     BrowserModule,
