a103f9988689045e0820d21a6a60c3a8193b751d
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-05-23 23:27:46 +0300

    Implement @Input for course item

--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Course } from 'src/app/interfaces/course.interface';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css'],
 })
-export class CourseCardComponent {}
+export class CourseCardComponent {
+  @Input() course: Course;
+}
