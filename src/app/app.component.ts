1db11f68368fd710cdfaf04041b94e4dea550521
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-05-23 22:58:49 +0300

    add mocked data of courses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { courses as data } from '../app/data/courses';
+import { Course } from './interfaces/course.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-courses-app';
+
+  courses: Course[] = data;
 }
---
+++ src/app/data/courses.ts
@@ -0,0 +1,10 @@
+export const courses = [
+  {
+    id: 1,
+    name: 'Name tag',
+    date: '11/02/2023',
+    length: 120,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+];
