1fed9bf3e1c970e9f927cceb32f7e96f1ab9f247
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-05-25 15:09:07 +0300

    add function to transform min to hours

--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -8,4 +8,15 @@ import { Course } from 'src/app/interfaces/course.interface';
 })
 export class CourseCardComponent {
   @Input() course: Course;
+
+  transformMinute(value: number): string {
+    let hours = Math.floor(value / 60);
+    let minutes = Math.floor(value % 60);
+
+    if (minutes > 0) {
+      return hours + 'h ' + (minutes < 10 ? '0' + minutes : minutes) + ' mins';
+    } else {
+      return hours + ' hours';
+    }
+  }
 }
