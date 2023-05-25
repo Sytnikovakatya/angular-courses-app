c954dc6a121fc54850c9cda58d795d315e2585d2
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-05-25 15:09:31 +0300

    Delete pipeDuration.ts

--- src/app/helpers/pipeDuration.ts
+++
@@ -1,5 +0,0 @@
-export default function pipeDuration(duration: number): string {
-  let hours = Math.trunc(duration / 60);
-  let minutes = duration % 60;
-  return hours + 'h' + (minutes < 10 ? '0' + minutes : minutes) + 'min';
-}
