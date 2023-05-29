eb0f312e4de4c3701ef2603ca347839664a4386d
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-05-29 15:39:11 +0300

    Update input.component.ts
    Add outPut with bindModeldata, place console.log statement in hooks

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,12 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
 })
-export class InputComponent {
+export class InputComponent implements OnChanges, OnInit {
+  @Input() search: string = '';
+
   @Input() placeholder: string = '';
   @Input() class: string = 'form-control';
   @Input() type: string = 'search';
+
+  @Input() bindModelData: unknown;
+  @Output() bindModelDataChange = new EventEmitter();
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  updateData(event: Event) {
+    this.bindModelData = event;
+    this.bindModelDataChange.emit(event);
+  }
+
+  ngOnInit() {
+    console.log('Init');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+  }
 }
