62d39a959244e82a0abde01880b9359a4a99d148
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-01 23:02:49 +0300

    Update header.component.spec.ts

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,3 +1,4 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
@@ -5,10 +6,24 @@ import { HeaderComponent } from './header.component';
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  @Component({
+    selector: 'app-logo',
+    template: '<div class="logo">Mock Logo Component</div>',
+  })
+  class MockLogoComponent {}
+  @Component({
+    selector: 'app-button',
+    template: '<div>Mock Button Component</div>',
+  })
+  class MockButtonComponent {
+    @Input() text: string;
+    @Input() fontawesome: string;
+    @Input() class: string;
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent],
+      declarations: [HeaderComponent, MockLogoComponent, MockButtonComponent],
     });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -17,5 +32,15 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render logo', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.logo')?.textContent).toContain('Logo');
+  });
+
+  it('should have username', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.user-login')?.textContent).toContain('User login');
   });
 });
