6af6d68cc8ecb35c63f0029d6ef337c843443b37
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-02 13:31:49 +0300

    Update search-bar.component.spec.ts

--- src/app/components/search-bar/search-bar.component.spec.ts
+++ src/app/components/search-bar/search-bar.component.spec.ts
@@ -1,14 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input } from '@angular/core';
+
+import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
 import { SearchBarComponent } from './search-bar.component';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
+  @Component({
+    selector: 'app-input',
+    template: '<div>Mock Input Component</div>',
+  })
+  class MockInputComponent {
+    @Input() placeholder: string;
+    @Input() bindModelData: string;
+  }
+  @Component({
+    selector: 'app-button',
+    template: '<div>Mock Button Component</div>',
+  })
+  class MockButtonComponent {
+    @Input() text: string;
+    @Input() type: string;
+    @Input() class: string;
+    @Input() fontawesome: string;
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SearchBarComponent],
+      declarations: [SearchBarComponent, MockInputComponent, MockButtonComponent],
     });
     fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
@@ -17,5 +37,12 @@ describe('SearchBarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call searchClick and print console.log with the searching value', () => {
+    spyOn(console, 'log');
+    component.search = 'hello';
+    component.searchClick();
+    expect(console.log).toHaveBeenCalledWith('hello');
   });
 });
