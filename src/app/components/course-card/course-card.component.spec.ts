8ad98a631e5ce483027a67c5571d7051cb9be209
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-03 14:05:31 +0300

    Update course-card.component.spec.ts

--- src/app/components/course-card/course-card.component.spec.ts
+++ src/app/components/course-card/course-card.component.spec.ts
@@ -18,4 +18,25 @@ describe('CourseCardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call delete method and print console.log', () => {
+    spyOn(console, 'log');
+    component.delete(1);
+    expect(console.log).toHaveBeenCalledWith('Delete №1');
+  });
+
+  it('should trasform duration of course to string', () => {
+    let mockDuration = component.transformMinute(110);
+    expect(mockDuration).toBe('1h 50 mins');
+  });
+
+  it('should trasform duration of course to string (if minutes < 0)', () => {
+    let mockDuration = component.transformMinute(120);
+    expect(mockDuration).toBe('2 hours');
+  });
+
+  it('should trasform duration of course to string (if minutes < 10)', () => {
+    let mockDuration = component.transformMinute(125);
+    expect(mockDuration).toBe('2h 05 mins');
+  });
 });
