aab8b3b1845c417a8eea6297b914efbe170b47e9
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-03 16:05:22 +0300

    Update search-bar.component.spec.ts
    delete import fakeAsync

--- src/app/components/search-bar/search-bar.component.spec.ts
+++ src/app/components/search-bar/search-bar.component.spec.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
-
-import { ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchBarComponent } from './search-bar.component';
 
