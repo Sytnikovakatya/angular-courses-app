4bc1e3fb64e84a2db746ef7f35c3ac96faf1f115
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-06 22:31:46 +0300

    Create duration pipe

---
+++ src/app/pipes/duration/duration.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'duration',
+})
+export class DurationPipe implements PipeTransform {
+  transform(value: number): string {
+    const hours: number = Math.floor(value / 60);
+    const minutes: number = Math.floor(value % 60);
+
+    if (hours === 0 && minutes < 59) {
+      return minutes + ' min';
+    } else if (minutes > 0) {
+      return hours + 'h ' + (minutes < 10 ? '0' + minutes : minutes) + ' min';
+    } else {
+      return hours + (hours === 1 ? ' hour' : ' hours');
+    }
+  }
+}
