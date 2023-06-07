8ad47615bd16a15338b570b5cea491bd91ab384d
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-07 23:51:14 +0300

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+
 import { Course } from '@interfaces/course.interface';
+
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   @Component({
