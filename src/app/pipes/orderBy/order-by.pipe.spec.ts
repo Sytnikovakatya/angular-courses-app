b9337cdf5871535512987254163b53e048368bb5
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-12 11:43:30 +0300

    Update order-by.pipe.spec.ts

--- src/app/pipes/orderBy/order-by.pipe.spec.ts
+++ src/app/pipes/orderBy/order-by.pipe.spec.ts
@@ -1,8 +1,30 @@
 import { OrderByPipe } from './order-by.pipe';
 
 describe('OrderByPipe', () => {
+  const pipe = new OrderByPipe();
+
   it('create an instance', () => {
-    const pipe = new OrderByPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return the array sorted in in descending order based on the date key', () => {
+    const mockCourses = [
+      {
+        id: 1,
+        name: 'name1',
+        date: 1666191470410,
+        length: 120,
+        description: 'description',
+      },
+      { id: 2, name: 'name2', date: 1696191470410, length: 120, description: 'description' },
+      { id: 3, name: 'name3', date: 1686191470410, length: 120, description: 'description' },
+    ];
+
+    const sortedCourses = pipe.transform(mockCourses);
+    expect(sortedCourses).toEqual([
+      { id: 2, name: 'name2', date: 1696191470410, length: 120, description: 'description' },
+      { id: 3, name: 'name3', date: 1686191470410, length: 120, description: 'description' },
+      { id: 1, name: 'name1', date: 1666191470410, length: 120, description: 'description' },
+    ]);
   });
 });
