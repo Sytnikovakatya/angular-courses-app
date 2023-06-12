b9d90528625f8983acceed31f31f04b75ca58717
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-12 11:10:39 +0300

    Update filter.pipe.ts

--- src/app/pipes/filter/filter.pipe.ts
+++ src/app/pipes/filter/filter.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import { Course } from '@interfaces/course.interface';
+
 @Pipe({
-  name: 'filter'
+  name: 'filter',
 })
 export class FilterPipe implements PipeTransform {
+  transform(value: Course[], filterString = ''): Course[] {
+    if (!value) return [];
+    if (!filterString) return value;
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    return value.filter(course => {
+      return JSON.stringify(course.name).toLowerCase().includes(filterString);
+    });
   }
-
 }
