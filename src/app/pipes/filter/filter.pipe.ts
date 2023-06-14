0f7162111bb40642ed34c117d528278a18606215
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-14 20:31:41 +0300

    add type in filter

--- src/app/pipes/filter/filter.pipe.ts
+++ src/app/pipes/filter/filter.pipe.ts
@@ -9,7 +9,7 @@ export class FilterPipe implements PipeTransform {
   transform(value: Course[], filterString: string): Course[] {
     if (!filterString) return value;
 
-    return value.filter(course => {
+    return value.filter((course: Course) => {
       return JSON.stringify(course.name).toLowerCase().includes(filterString);
     });
   }
