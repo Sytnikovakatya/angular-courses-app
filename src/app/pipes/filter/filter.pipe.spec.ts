c213c9a2e8174a3f8fb85c1147ea872d086989a7
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-15 18:50:10 +0300

    change dates in mock components on the type string

--- src/app/pipes/filter/filter.pipe.spec.ts
+++ src/app/pipes/filter/filter.pipe.spec.ts
@@ -9,29 +9,29 @@ describe('FilterPipe', () => {
 
   it('should filter courses based on a provided search term', () => {
     const mockCourses = [
-      { id: 1, name: 'name1', date: 1666191470410, length: 120, description: 'description' },
-      { id: 2, name: 'name2', date: 1696191470410, length: 120, description: 'description' },
-      { id: 3, name: 'name3', date: 1686191470410, length: 120, description: 'description' },
+      { id: 1, name: 'name1', date: '2023-06-14T04:39:24+00:00', length: 120, description: 'description' },
+      { id: 2, name: 'name2', date: '2023-03-14T04:39:24+00:00', length: 120, description: 'description' },
+      { id: 3, name: 'name3', date: '2023-06-10T04:39:24+00:00', length: 120, description: 'description' },
     ];
     const searchTerm = 'name1';
     const filteredArray = pipe.transform(mockCourses, searchTerm);
     expect(filteredArray).toEqual([
-      { id: 1, name: 'name1', date: 1666191470410, length: 120, description: 'description' },
+      { id: 1, name: 'name1', date: '2023-06-14T04:39:24+00:00', length: 120, description: 'description' },
     ]);
   });
 
   it('should not filter an array if the search term is empty', () => {
     const mockCourses = [
-      { id: 1, name: 'name1', date: 1666191470410, length: 120, description: 'description' },
-      { id: 2, name: 'name2', date: 1696191470410, length: 120, description: 'description' },
-      { id: 3, name: 'name3', date: 1686191470410, length: 120, description: 'description' },
+      { id: 1, name: 'name1', date: '2023-06-14T04:39:24+00:00', length: 120, description: 'description' },
+      { id: 2, name: 'name2', date: '2023-03-14T04:39:24+00:00', length: 120, description: 'description' },
+      { id: 3, name: 'name3', date: '2023-06-10T04:39:24+00:00', length: 120, description: 'description' },
     ];
     const searchTerm = '';
     const filteredArray = pipe.transform(mockCourses, searchTerm);
     expect(filteredArray).toEqual([
-      { id: 1, name: 'name1', date: 1666191470410, length: 120, description: 'description' },
-      { id: 2, name: 'name2', date: 1696191470410, length: 120, description: 'description' },
-      { id: 3, name: 'name3', date: 1686191470410, length: 120, description: 'description' },
+      { id: 1, name: 'name1', date: '2023-06-14T04:39:24+00:00', length: 120, description: 'description' },
+      { id: 2, name: 'name2', date: '2023-03-14T04:39:24+00:00', length: 120, description: 'description' },
+      { id: 3, name: 'name3', date: '2023-06-10T04:39:24+00:00', length: 120, description: 'description' },
     ]);
   });
 });
--- src/app/pipes/orderBy/order-by.pipe.spec.ts
+++ src/app/pipes/orderBy/order-by.pipe.spec.ts
@@ -9,16 +9,16 @@ describe('OrderByPipe', () => {
 
   it('should return the array sorted in in descending order based on the date key', () => {
     const mockCourses = [
-      { id: 1, name: 'name1', date: 1666191470410, length: 120, description: 'description' },
-      { id: 2, name: 'name2', date: 1696191470410, length: 120, description: 'description' },
-      { id: 3, name: 'name3', date: 1686191470410, length: 120, description: 'description' },
+      { id: 1, name: 'name1', date: '2023-03-14T04:39:24+00:00', length: 120, description: 'description' },
+      { id: 2, name: 'name2', date: '2023-06-14T04:39:24+00:00', length: 120, description: 'description' },
+      { id: 3, name: 'name3', date: '2023-06-10T04:39:24+00:00', length: 120, description: 'description' },
     ];
 
     const sortedCourses = pipe.transform(mockCourses);
     expect(sortedCourses).toEqual([
-      { id: 2, name: 'name2', date: 1696191470410, length: 120, description: 'description' },
-      { id: 3, name: 'name3', date: 1686191470410, length: 120, description: 'description' },
-      { id: 1, name: 'name1', date: 1666191470410, length: 120, description: 'description' },
+      { id: 2, name: 'name2', date: '2023-06-14T04:39:24+00:00', length: 120, description: 'description' },
+      { id: 3, name: 'name3', date: '2023-06-10T04:39:24+00:00', length: 120, description: 'description' },
+      { id: 1, name: 'name1', date: '2023-03-14T04:39:24+00:00', length: 120, description: 'description' },
     ]);
   });
 });
