fae9fac815b9e227a3954668c5caa75cec39a33a
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-15 18:51:20 +0300

    Update highlight.directive.spec.ts

--- src/app/directives/hightlight/highlight.directive.spec.ts
+++ src/app/directives/hightlight/highlight.directive.spec.ts
@@ -6,10 +6,15 @@ import { By } from '@angular/platform-browser';
 import { HighlightDirective } from './highlight.directive';
 
 @Component({
-  template: ` <div appHighlight="red">Highlight me</div>
-    <h2 appHighlight>No Highlight</h2>`,
+  template: ` <div [appHighlight]="today">Highlight me</div>
+    <p [appHighlight]="futureDate">Highlight me</p>
+    <h2 [appHighlight]="pastDate">No Highlight</h2>`,
 })
-class TestComponent {}
+class TestComponent {
+  today: string = new Date().toString();
+  futureDate: string = new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toString();
+  pastDate = new Date(new Date().getTime() - 20 * 24 * 60 * 60 * 1000).toString();
+}
 
 describe('HighlightDirective', () => {
   let component: TestComponent;
@@ -31,14 +36,19 @@ describe('HighlightDirective', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should apply highlight color to the element', () => {
+  it('should set border color to lightgreen if the date is within the last 14 days', () => {
     divElement = fixture.debugElement.query(By.css('div')).nativeElement;
-    expect(divElement.style.borderColor).toBe('red');
+    expect(divElement.style.borderColor).toBe('lightgreen');
   });
 
-  it('should not apply highlight color if input is not provided', () => {
+  it('should set border color to skyblue if the date is in the future', () => {
+    divElement = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(divElement.style.borderColor).toBe('skyblue');
+  });
+
+  it('should set border color to lightgrey if the date is in the past and more than 14 days ago', () => {
     newElement = fixture.debugElement.query(By.css('h2')).nativeElement;
     fixture.detectChanges();
-    expect(newElement.style.borderColor).toBeFalsy();
+    expect(newElement.style.borderColor).toBe('lightgrey');
   });
 });
