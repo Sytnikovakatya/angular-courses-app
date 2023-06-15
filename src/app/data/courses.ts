db228090eab5a116701947ca3e943cfd05f80759
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-15 15:03:43 +0300

    Update courses.ts

--- src/app/data/courses.ts
+++ src/app/data/courses.ts
@@ -18,7 +18,7 @@ export const courses = [
   {
     id: 3,
     name: 'Typescript',
-    date: 1686191470410,
+    date: 1706191470410,
     length: 140,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
