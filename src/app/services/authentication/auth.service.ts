f189adfbd85ba8131fc52199ff97b019b85ecfa8
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-19 17:08:46 +0300

    Implement new methods in authenticated service

--- src/app/services/authentication/auth.service.ts
+++ src/app/services/authentication/auth.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
 
+import { Subject, Observable } from 'rxjs';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private _eventSubject: Subject<boolean> = new Subject();
+  public event$: Observable<boolean> = this._eventSubject.asObservable();
+  constructor() {}
 
-  constructor() { }
+  login(userEmail: string): void {
+    if (userEmail) {
+      console.log('Logged in successful!');
+
+      localStorage.setItem('name', userEmail);
+      localStorage.setItem('authenticated', 'true');
+    } else {
+      window.alert('Please, enter your email and password');
+    }
+
+    this._eventSubject.next(true);
+  }
+
+  logout(): void {
+    localStorage.removeItem('name');
+    localStorage.setItem('authenticated', 'false');
+
+    this._eventSubject.next(false);
+  }
+
+  isAuthenticated(): boolean {
+    const authenticated = localStorage.getItem('authenticated') === 'true';
+    return authenticated;
+  }
+
+  getUserInfo(): string | null {
+    return localStorage.getItem('name');
+  }
 }
