f1df5ae43f5d16b9b8df3aaba11cc138373d751b
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-19 17:17:46 +0300

    Bind inputs to class properties, add handler login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from '@services/authentication/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   validation = true;
+  email = '';
+  password = '';
+
+  constructor(public authService: AuthService) {}
+
+  login(email: string): void {
+    this.authService.login(email);
+    window.location.reload();
+  }
 }
