da39b2f4fc7975d5da9824b36f0b9801b26f7b22
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-19 17:07:49 +0300

    Implement action on logout, add services to check user info

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from '@services/authentication/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  user: string | null = '';
+  authentificated = false;
+
+  constructor(public authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authentificated = this.authService.isAuthenticated();
+    this.user = this.authService.getUserInfo();
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.authentificated = !this.authentificated;
+    window.location.reload();
+  }
+}
