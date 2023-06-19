c91a34b7ccfde2a3793fa984a2a6cf10df4acb54
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-19 17:15:57 +0300

    Update if-authenticated.directive.ts

--- src/app/directives/ifAuthenticated/if-authenticated.directive.ts
+++ src/app/directives/ifAuthenticated/if-authenticated.directive.ts
@@ -9,10 +9,10 @@ export class IfAuthenticatedDirective {
   constructor(private templateRef: TemplateRef<unknown>, private viewContainer: ViewContainerRef) {}
 
   @Input() set appIfAuthenticated(condition: boolean) {
-    if (!condition && !this.hasView) {
+    if (condition && !this.hasView) {
       this.viewContainer.createEmbeddedView(this.templateRef);
       this.hasView = true;
-    } else if (condition && this.hasView) {
+    } else if (!condition && this.hasView) {
       this.viewContainer.clear();
       this.hasView = false;
     }
