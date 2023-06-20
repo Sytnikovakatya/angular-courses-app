50dca2bddb709a7b330b7c7abd941b6c9590587d
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-20 13:11:29 +0300

    Update if-authenticated.directive.spec.ts

--- src/app/directives/ifAuthenticated/if-authenticated.directive.spec.ts
+++ src/app/directives/ifAuthenticated/if-authenticated.directive.spec.ts
@@ -1,8 +1,29 @@
+import { MockBuilder, MockRender } from 'ng-mocks';
+
 import { IfAuthenticatedDirective } from './if-authenticated.directive';
 
 describe('IfAuthenticatedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IfAuthenticatedDirective();
-    expect(directive).toBeTruthy();
+  beforeEach(() => MockBuilder(IfAuthenticatedDirective));
+
+  it('should hide and render its content', () => {
+    const fixture = MockRender(
+      `
+        <div *appIfAuthenticated="authenticated">
+          content
+        </div>
+    `,
+      {
+        authenticated: false,
+      }
+    );
+    expect(fixture.nativeElement.innerHTML).not.toContain('content');
+
+    fixture.componentInstance.authenticated = true;
+    fixture.detectChanges();
+    expect(fixture.nativeElement.innerHTML).toContain('content');
+
+    fixture.componentInstance.authenticated = false;
+    fixture.detectChanges();
+    expect(fixture.nativeElement.innerHTML).not.toContain('content');
   });
 });
