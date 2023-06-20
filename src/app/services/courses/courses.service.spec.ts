8bfd3a30362a94718461d3f2a20aab254a09ddaa
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-20 13:46:32 +0300

    Update courses.service.spec.ts

--- src/app/services/courses/courses.service.spec.ts
+++ src/app/services/courses/courses.service.spec.ts
@@ -1,16 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { MockBuilder, MockRender } from 'ng-mocks';
 
 import { CoursesService } from './courses.service';
 
-describe('CoursesService', () => {
-  let service: CoursesService;
+import { Course } from '@interfaces/course.interface';
+import { courses } from '@data/courses';
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CoursesService);
+describe('CoursesService', () => {
+  beforeEach(() => MockBuilder(CoursesService));
+
+  it('should return all courses', () => {
+    const service = MockRender(CoursesService).point.componentInstance;
+    expect(service.getAll()).toEqual(courses);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should add a new course to the list', () => {
+    const newItem: Course = {
+      id: 4,
+      name: 'New Course',
+      date: new Date().toISOString(),
+      length: 120,
+      description: 'New course description',
+    };
+
+    const service = MockRender(CoursesService).point.componentInstance;
+    service.addToCourses(newItem);
+    expect(service.courses).toContain(newItem);
   });
 });
