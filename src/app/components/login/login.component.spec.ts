b24d3dc5b376fb1b56e411c859f99daf5572d616
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-20 13:46:50 +0300

    Update login.component.spec.ts

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,6 +1,27 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+
+@Component({
+  selector: 'app-input',
+  template: '<div>Mock Input Component</div>',
+})
+class MockInputComponent {
+  @Input() type: string;
+  @Input() placeholder: string;
+  @Input() bindModelData: string;
+}
+
+@Component({
+  selector: 'app-button',
+  template: '<div>Mock Button Component</div>',
+})
+class MockButtonComponent {
+  @Input() text: string;
+  @Input() type: string;
+  @Input() class: string;
+}
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +29,7 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent, MockInputComponent, MockButtonComponent],
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
