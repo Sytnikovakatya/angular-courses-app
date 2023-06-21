1db6d81024e299ea20c27b81a315c0412d86fed5
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-21 17:02:03 +0300

    Update courses.service.ts
    delete variable course

--- src/app/services/courses/courses.service.ts
+++ src/app/services/courses/courses.service.ts
@@ -8,36 +8,34 @@ import { courses } from '@data/courses';
   providedIn: 'root',
 })
 export class CoursesService {
-  courses: Course[] = [];
-
   getCourses(): Course[] {
-    return (this.courses = courses);
+    return courses;
   }
 
   addToCourses(newItem: Course): void {
-    if (this.courses.some(course => course.id === newItem.id)) {
+    if (courses.some(course => course.id === newItem.id)) {
       window.alert('This Video course already exists!');
     } else {
-      this.courses.push(newItem);
+      courses.push(newItem);
     }
   }
 
   getCourseById(id: number): void {
-    const chosenCourse = this.courses.filter((course: Course) => course.id === id);
+    const chosenCourse = courses.filter((course: Course) => course.id === id);
     console.log(chosenCourse[0]);
   }
 
   updateCourse(item: Course): void {
-    this.courses.forEach((course, index) => {
+    courses.forEach((course, index) => {
       if (course.id === item.id) {
-        this.courses[index] = { ...course, name: 'New name', description: 'New Description' };
+        courses[index] = { ...course, name: 'New name', description: 'New Description' };
       }
     });
   }
 
   removeCourse(id: number): Course[] {
-    return (this.courses = this.courses.filter(course => {
+    return courses.filter(course => {
       return course.id !== id;
-    }));
+    });
   }
 }
