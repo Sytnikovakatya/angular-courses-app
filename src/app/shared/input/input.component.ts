f6a5e34e91d13d2f40a51c4d0cc533e3bccda128
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-23 10:49:52 +0300

    Update input.component.ts

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class InputComponent {
   @Input() placeholder: string = '';
   @Input() class: string = 'form-control';
-  @Input() type: string = 'search';
+  @Input() type: string = 'text';
 
   @Input() bindModelData: string;
   @Output() bindModelDataChange = new EventEmitter<string>();
