5003f823d5e6f7ed389847145ad4e38d2e93bd7f
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-23 11:00:03 +0300

    add mock components

--- src/app/components/add-course/add-course.component.spec.ts
+++ src/app/components/add-course/add-course.component.spec.ts
@@ -1,6 +1,30 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule } from '@angular/forms';
+
 import { AddCourseComponent } from './add-course.component';
+
+@Component({
+  selector: 'app-input',
+  template: '<div>Mock Input Component</div>',
+})
+class MockInputComponent {
+  @Input() placeholder: string;
+  @Input() bindModelData: string;
+  @Input() type: string;
+}
+
+@Component({
+  selector: 'app-button',
+  template: '<div>Mock Button Component</div>',
+})
+class MockButtonComponent {
+  @Input() text: string;
+  @Input() type: string;
+  @Input() class: string;
+  @Input() fontawesome: string;
+}
 
 describe('AddCourseComponent', () => {
   let component: AddCourseComponent;
@@ -8,7 +32,8 @@ describe('AddCourseComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AddCourseComponent]
+      imports: [FormsModule],
+      declarations: [AddCourseComponent, MockInputComponent, MockButtonComponent],
     });
     fixture = TestBed.createComponent(AddCourseComponent);
     component = fixture.componentInstance;
