7b21d46b91fac153699f8a1bc60351f3754f6e70
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-23 12:08:53 +0300

    cretae markup for Authors component

--- src/app/components/authors/authors.component.ts
+++ src/app/components/authors/authors.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
-  styleUrls: ['./authors.component.css']
+  styleUrls: ['./authors.component.css'],
 })
 export class AuthorsComponent {
-
+  authors = '';
 }
