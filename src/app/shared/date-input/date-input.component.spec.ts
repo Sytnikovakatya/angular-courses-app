9461cbca326d5097bf37c1df04fea15f99acf3ca
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-23 12:55:17 +0300

    add FormsModule import

--- src/app/shared/date-input/date-input.component.spec.ts
+++ src/app/shared/date-input/date-input.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule } from '@angular/forms';
+
 import { DateInputComponent } from './date-input.component';
 
 describe('DateInputComponent', () => {
@@ -8,7 +10,8 @@ describe('DateInputComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DateInputComponent]
+      imports: [FormsModule],
+      declarations: [DateInputComponent],
     });
     fixture = TestBed.createComponent(DateInputComponent);
     component = fixture.componentInstance;
