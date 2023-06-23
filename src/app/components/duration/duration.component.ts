1709c33ff17d2f55d9c45e5af4d4d5fbfb775201
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-23 12:34:30 +0300

    Update duration.component.ts

--- src/app/components/duration/duration.component.ts
+++ src/app/components/duration/duration.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-duration',
   templateUrl: './duration.component.html',
-  styleUrls: ['./duration.component.css']
+  styleUrls: ['./duration.component.css'],
 })
 export class DurationComponent {
+  @Input() bindModelData: number;
+  @Output() bindModelDataChange = new EventEmitter<number>();
 
+  updateData(event: number): void {
+    this.bindModelData = event;
+    this.bindModelDataChange.emit(event);
+  }
 }
