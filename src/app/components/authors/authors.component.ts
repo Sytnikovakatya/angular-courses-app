84826c321edb5e8bd708fe80995e7dd41f579658
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-24 10:51:21 +0300

    Update authors.component.ts

--- src/app/components/authors/authors.component.ts
+++ src/app/components/authors/authors.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Author } from '@interfaces/author';
 
 @Component({
   selector: 'app-authors',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./authors.component.css'],
 })
 export class AuthorsComponent {
-  authors = '';
+  authorName = '';
+  authors: Author[] = [];
 }
