1556dd9f4cc5b111b6931387187fac31fb64ed59
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-26 12:57:19 +0300

    Update header.component.ts

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from 'app/core/services/authentication/auth.service';
+import { AuthService } from '@services/authentication/auth.service';
 
 @Component({
   selector: 'app-header',
