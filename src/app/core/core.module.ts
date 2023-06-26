427b450a22581378ea2fd30645743b55841701fa
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-26 12:57:52 +0300

    Update core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '@shared/shared.module';
+
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-
-import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent],
