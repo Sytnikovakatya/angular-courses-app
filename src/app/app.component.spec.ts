1b0031ea4dce20f5e00c931aad72ff62b4de017a
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-26 13:19:59 +0300

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { OrderByPipe } from '@pipes/orderBy/order-by.pipe';
-import { FilterPipe } from '@pipes/filter/filter.pipe';
-
-import { IfAuthenticatedDirective } from '@directives/ifAuthenticated/if-authenticated.directive';
-
+import { SharedModule } from '@shared/shared.module';
 import { Course } from '@interfaces/course.interface';
 import { courses } from '@data/courses';
 
@@ -71,7 +66,7 @@ describe('AppComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, SharedModule],
       declarations: [
         AppComponent,
         MockHeaderComponent,
@@ -82,9 +77,6 @@ describe('AppComponent', () => {
         MockLoaderComponent,
         MockCourseCardComponent,
         MockLoginComponent,
-        OrderByPipe,
-        FilterPipe,
-        IfAuthenticatedDirective,
       ],
     });
     fixture = TestBed.createComponent(AppComponent);
