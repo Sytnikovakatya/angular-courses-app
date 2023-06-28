7b2bcd8259618c682b0310698e82dcf88413d37e
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-28 20:47:39 +0300

    Update app.component.ts
    remove methods to CourseListComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Course } from '@interfaces/course.interface';
-
-import { CoursesService } from '@services/courses/courses.service';
-import { AuthService } from '@services/authentication/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = 'angular-courses-app';
-  authentificated = false;
-  filterBy = '';
-  courses: Course[] = [];
-
-  constructor(private coursesService: CoursesService, private authService: AuthService) {}
-
-  ngOnInit(): void {
-    this.authentificated = this.authService.isAuthenticated();
-    this.courses = this.coursesService.getCourses();
-  }
-
-  courseTrackBy(index: number, course: Course): number {
-    return course.id;
-  }
-
-  getSearchValue(newValue: string): string {
-    return (this.filterBy = newValue);
-  }
-}
+export class AppComponent {}
