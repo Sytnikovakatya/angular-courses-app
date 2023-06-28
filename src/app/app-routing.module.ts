32ce1e58b7ddfb48b3cde2801cd9bb8f7ebf6aa9
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-28 20:44:08 +0300

    implement routes, use lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from '@shared/components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
+  { path: 'courses', loadChildren: () => import('./modules/courses/courses.module').then(m => m.CoursesModule) },
+  {
+    path: 'courses/:id',
+    loadChildren: () => import('./modules/course-form/course-form.module').then(m => m.CourseFormModule),
+  },
+  {
+    path: 'courses/new',
+    loadChildren: () => import('./modules/course-form/course-form.module').then(m => m.CourseFormModule),
+  },
+
+  { path: '**', component: NotFoundComponent, data: { breadcrumb: 'Courses' } },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
