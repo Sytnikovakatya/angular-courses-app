8928b636791be1a972f19ea8359fada0b1093b98
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-29 15:24:25 +0300

    Update auth.guard.spec.ts

--- src/app/core/guards/auth.guard.spec.ts
+++ src/app/core/guards/auth.guard.spec.ts
@@ -1,17 +1,44 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from '@services/authentication/auth.service';
+import { Observable, of } from 'rxjs';
 
-import { authGuard } from './auth.guard';
-
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [AuthGuard, AuthService],
+    });
+    guard = TestBed.inject(AuthGuard);
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
   });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+  it('should allow navigation when user is authenticated', () => {
+    spyOn(authService, 'isAuthenticated').and.returnValue(true);
+    const routeSnapshot: ActivatedRouteSnapshot = new ActivatedRouteSnapshot();
+    const stateSnapshot: RouterStateSnapshot = jasmine.createSpyObj<RouterStateSnapshot>('RouterStateSnapshot', [
+      'toString',
+    ]);
+    const result = guard.canActivate(routeSnapshot, stateSnapshot);
+    expect(result).toBeTrue();
+  });
+
+  it('should navigate to login page and deny navigation when user is not authenticated', () => {
+    spyOn(authService, 'isAuthenticated').and.returnValue(false);
+    const routeSnapshot: ActivatedRouteSnapshot = new ActivatedRouteSnapshot();
+    const stateSnapshot: RouterStateSnapshot = jasmine.createSpyObj<RouterStateSnapshot>('RouterStateSnapshot', [
+      'toString',
+    ]);
+    const navigateSpy = spyOn(router, 'navigate');
+    const result = guard.canActivate(routeSnapshot, stateSnapshot);
+    expect(navigateSpy).toHaveBeenCalledWith(['/login']);
+    expect(result).toBeFalse();
   });
 });
