2c667c0a531aa9c77a83120229220de34e902da6
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-29 13:23:19 +0300

    Update app-routing.module.ts
    add authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+
+import { LoginComponent } from '@components/login/login/login.component';
 
 import { NotFoundComponent } from '@shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'courses', pathMatch: 'full' },
-  { path: 'courses', loadChildren: () => import('./modules/courses/courses.module').then(m => m.CoursesModule) },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'courses',
+    loadChildren: () => import('./modules/courses/courses.module').then(m => m.CoursesModule),
+    canActivate: [AuthGuard],
+  },
   {
     path: 'courses/:id',
     loadChildren: () => import('./modules/course-form/course-form.module').then(m => m.CourseFormModule),
