68ea40ca3ea03058d6fca3db660ecd198666589f
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-06-29 15:24:53 +0300

    Update course-list.component.spec.ts

--- src/app/modules/courses/course-list/course-list.component.spec.ts
+++ src/app/modules/courses/course-list/course-list.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { courses } from '@data/courses';
+
 import { CourseListComponent } from './course-list.component';
 
 describe('CourseListComponent', () => {
@@ -17,5 +19,18 @@ describe('CourseListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should get the id of course', () => {
+    const result = component.courseTrackBy(1, courses[0]);
+    fixture.detectChanges();
+    expect(result).toBe(courses[0].id);
+  });
+
+  it('should update the filterBy value', () => {
+    const searchValue = 'Angular';
+
+    component.getSearchValue(searchValue);
+
+    expect(component.filterBy).toBe(searchValue);
   });
 });
