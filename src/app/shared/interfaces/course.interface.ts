2ddde115d40e539a54503ddc3125853dc0810a5d
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-02 22:23:24 +0300

    Update course.interface.ts

--- src/app/shared/interfaces/course.interface.ts
+++ src/app/shared/interfaces/course.interface.ts
@@ -1,7 +1,11 @@
+import { Author } from './author';
+
 export interface Course {
   id: number;
   name: string;
   date: string;
   length: number;
   description: string;
+  authors: Author[];
+  isTopRated: boolean;
 }
