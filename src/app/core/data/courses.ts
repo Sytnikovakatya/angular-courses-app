1895b46413c1b1ec2901c4f9e8378dd8f60bd3fc
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-02 22:25:37 +0300

    Update courses.ts

--- src/app/core/data/courses.ts
+++ src/app/core/data/courses.ts
@@ -4,23 +4,47 @@ export const courses = [
     name: 'Javascript',
     date: '2017-09-28T04:39:24+00:00',
     length: 120,
+    isTopRated: false,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    authors: [
+      {
+        id: 8413,
+        name: 'Greta',
+        lastName: 'Richardson',
+      },
+    ],
   },
   {
     id: 2,
     name: 'Angular',
     date: '2023-06-14T04:39:24+00:00',
     length: 220,
+    isTopRated: false,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    authors: [
+      {
+        id: 8413,
+        name: 'Greta',
+        lastName: 'Richardson',
+      },
+    ],
   },
   {
     id: 3,
     name: 'Typescript',
     date: '2023-09-28T04:39:24+00:00',
     length: 140,
+    isTopRated: false,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    authors: [
+      {
+        id: 8413,
+        name: 'Greta',
+        lastName: 'Richardson',
+      },
+    ],
   },
 ];
