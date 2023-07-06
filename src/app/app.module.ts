76f6a5c36738d9796256769a08a98dd4a6c6e1f3
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-06 16:01:01 +0300

    Provide the interceptor in your module's providers array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,6 +12,7 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 
 import { SharedModule } from '@shared/shared.module';
 import { CoreModule } from '@core/core.module';
+import { TokenInterceptor } from '@services/authentication/interceptor/token.interceptor';
 
 import { LoginModule } from '@components/login/login.module';
 import { BreadcrumbsModule } from '@components/breadcrumbs/breadcrumbs.module';
@@ -32,7 +33,14 @@ import { AppComponent } from './app.component';
     LoginModule,
     BreadcrumbsModule,
   ],
-  providers: [BreadcrumbService],
+  providers: [
+    BreadcrumbService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
