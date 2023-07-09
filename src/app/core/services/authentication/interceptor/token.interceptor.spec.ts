37686b84433c08d98b6f45f791c167744cd88816
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-09 23:46:12 +0300

    Update token.interceptor.spec.ts

--- src/app/core/services/authentication/interceptor/token.interceptor.spec.ts
+++ src/app/core/services/authentication/interceptor/token.interceptor.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 import { TokenInterceptor } from './token.interceptor';
 
 describe('TokenInterceptorInterceptor', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [TokenInterceptor],
     })
   );
