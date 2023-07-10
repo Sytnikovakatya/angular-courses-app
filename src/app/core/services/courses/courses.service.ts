e281b8968b4f43e72de227d04c115ed2b28173da
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-10 22:19:00 +0300

    delete handleerror method

--- src/app/core/services/courses/courses.service.ts
+++ src/app/core/services/courses/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { Course } from '@shared/interfaces/course.interface';
 
@@ -18,47 +18,34 @@ export class CoursesService {
   constructor(private http: HttpClient) {}
 
   getCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>(this.apiUrl + '?start=0&count=5').pipe(catchError(this.handleError));
+    return this.http.get<Course[]>(this.apiUrl + '?start=0&count=5');
   }
 
   loadMoreCourses(amount: number): Observable<Course[]> {
-    return this.http.get<Course[]>(this.apiUrl + `?start=0&count=${amount}`).pipe(catchError(this.handleError));
+    return this.http.get<Course[]>(this.apiUrl + `?start=0&count=${amount}`);
   }
 
   searchCourse(term: string): Observable<Course[]> {
-    return this.http.get<Course[]>(this.apiUrl + `?textFragment=${term}`).pipe(catchError(this.handleError));
+    return this.http.get<Course[]>(this.apiUrl + `?textFragment=${term}`);
   }
 
   orderCourses(value: string): Observable<Course[]> {
-    return this.http.get<Course[]>(this.apiUrl + `?sort=${value}`).pipe(catchError(this.handleError));
+    return this.http.get<Course[]>(this.apiUrl + `?sort=${value}`);
   }
 
   createCourse(newItem: Course): Observable<Course> {
-    return this.http.post<Course>(this.apiUrl, newItem).pipe(catchError(this.handleError));
+    return this.http.post<Course>(this.apiUrl, newItem);
   }
 
   getCourseById(id: number): Observable<Course> {
-    return this.http.get<Course>(this.apiUrl + `/${id}`).pipe(catchError(this.handleError));
+    return this.http.get<Course>(this.apiUrl + `/${id}`);
   }
 
   updateCourse(id: number, course: Course): Observable<Course> {
-    return this.http.patch<Course>(this.apiUrl + `/${id}`, course, this.httpOptions).pipe(catchError(this.handleError));
+    return this.http.patch<Course>(this.apiUrl + `/${id}`, course, this.httpOptions);
   }
 
   removeCourse(id: number): Observable<Course> {
-    return this.http.delete<Course>(this.apiUrl + `/${id}`).pipe(catchError(this.handleError));
-  }
-
-  handleError(error: { error: { message: string }; status: number }) {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      errorMessage = error.error.message;
-    } else {
-      errorMessage = `Error Code: ${error.status}`;
-    }
-    window.alert(errorMessage);
-    return throwError(() => {
-      return errorMessage;
-    });
+    return this.http.delete<Course>(this.apiUrl + `/${id}`);
   }
 }
