7d2d73141e780c8edcb4cdeb25068e8d96be2642
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-10 22:18:24 +0300

    implement error interceptor

--- src/app/core/interceptors/error/error.interceptor.ts
+++ src/app/core/interceptors/error/error.interceptor.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor() {}
-
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        window.alert(errorMessage);
+        return throwError(() => {
+          return errorMessage;
+        });
+      })
+    );
   }
 }
