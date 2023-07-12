70095b72364b8caf84b8bc6d35395bff501e6b99
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-12 19:27:52 +0300

    Update auth.guard.ts

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -4,9 +4,13 @@ import { Router, UrlTree } from '@angular/router';
 import { AuthService } from '@services/authentication/auth.service';
 
 export function authGuard(): boolean | UrlTree {
+  let authenticated = false;
   const authService = inject(AuthService);
   const router = inject(Router);
-  if (authService.isAuthenticated()) {
+
+  authService.isAuthenticated.subscribe(auth => (authenticated = auth));
+
+  if (authenticated) {
     return true;
   }
   return router.parseUrl('/login');
