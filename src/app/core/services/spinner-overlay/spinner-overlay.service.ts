6cfee47e634ae106bd8f6be4f7863867422a5589
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-12 23:10:58 +0300

    Update spinner-overlay.service.ts
    add types

--- src/app/core/services/spinner-overlay/spinner-overlay.service.ts
+++ src/app/core/services/spinner-overlay/spinner-overlay.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpinnerOverlayService {
-  private loading = new BehaviorSubject<boolean>(false);
-  public readonly loading$ = this.loading.asObservable();
+  private loading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public readonly loading$: Observable<boolean> = this.loading.asObservable();
 
   show() {
     this.loading.next(true);
