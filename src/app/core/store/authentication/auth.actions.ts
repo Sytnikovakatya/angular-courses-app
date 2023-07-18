ab093a6590cc2131b408034be8e1fae7217350a8
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-18 15:33:52 +0300

    Create auth.actions.ts

---
+++ src/app/core/store/authentication/auth.actions.ts
@@ -0,0 +1,13 @@
+import { createAction, props } from '@ngrx/store';
+
+import { Token } from '@shared/interfaces/token.interface';
+import { User } from '@shared/interfaces/user.interface';
+
+export const login = createAction('[Auth] Login', props<{ credentials: { login: string; password: string } }>());
+export const loginSuccess = createAction('[Auth] Login Success', props<{ token: Token }>());
+export const loginFailure = createAction('[Auth] Login Failure', props<{ errorMsg: string }>());
+
+export const logout = createAction('[Auth] Logout');
+
+export const getUserInfo = createAction('[Auth] Get User Info', props<{ user: User }>());
+export const getUserInfoFailure = createAction('[Auth] Get User Info Failure', props<{ errorMsg: string }>());
