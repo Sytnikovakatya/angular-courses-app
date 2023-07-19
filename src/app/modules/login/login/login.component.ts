f3ccba4e4a9126e5711d1fdf1702aab3446e6fdf
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-19 20:30:53 +0300

    Update login.component.ts
    add error message

--- src/app/modules/login/login/login.component.ts
+++ src/app/modules/login/login/login.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
 
 import { Store } from '@ngrx/store';
 
 import { AppState } from '@store/app.state';
 import * as AuthActions from '@store/authentication/auth.actions';
+import { selectErrorMsg } from '@store/authentication/auth.selectors';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   validation = true;
   email = '';
   password = '';
 
-  constructor(private store: Store<AppState>) {}
+  getState$: Observable<string | null>;
+  errorMessage: string | null;
+
+  constructor(private store: Store<AppState>) {
+    this.getState$ = this.store.select(selectErrorMsg);
+  }
+
+  ngOnInit(): void {
+    this.getState$.subscribe(err => {
+      this.errorMessage = err;
+    });
+  }
 
   login(): void {
     const credentials = {
