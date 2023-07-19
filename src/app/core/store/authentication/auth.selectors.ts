3bcf299004ca1c9361431e4f5c67d842b5bafed8
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-20 01:52:59 +0300

    Update auth.selectors.ts

--- src/app/core/store/authentication/auth.selectors.ts
+++ src/app/core/store/authentication/auth.selectors.ts
@@ -4,4 +4,6 @@ import { AuthState } from './auth.reducer';
 
 export const selectAuthState = (state: AppState) => state.auth;
 
+export const selectUserInfo = createSelector(selectAuthState, (state: AuthState) => state.user);
+export const selectIsAuthenticated = createSelector(selectAuthState, (state: AuthState) => state.isAuthenticated);
 export const selectErrorMsg = createSelector(selectAuthState, (state: AuthState) => state.error);
