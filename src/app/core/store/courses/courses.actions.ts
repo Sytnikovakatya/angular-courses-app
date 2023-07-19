c39138d1434960de969b0c17046a61cc3dbbbec8
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-20 00:15:05 +0300

    Update courses.actions.ts

--- src/app/core/store/courses/courses.actions.ts
+++ src/app/core/store/courses/courses.actions.ts
@@ -1,10 +1,31 @@
 import { createAction, props } from '@ngrx/store';
 import { Course } from '@shared/interfaces/course.interface';
 
-export const setCourses = createAction('[Courses] Set Courses', props<{ courses: Course[] }>());
-export const searchCourses = createAction('[Courses] Search Courses', props<{ courses: Course[] }>());
-export const sortCourses = createAction('[Courses] Sort Courses', props<{ courses: Course[] }>());
-export const loadMoreCourses = createAction('[Courses] Load More Courses', props<{ courses: Course[] }>());
+export const setCourses = createAction('[Courses] Set Courses');
+export const setCoursesSuccess = createAction('[Courses] Set Courses Success', props<{ courses: Course[] }>());
+export const setCoursesFailure = createAction('[Courses] Set Courses Failure', props<{ errorMsg: string }>());
+
+export const getCourse = createAction('[Courses] Get Course', props<{ id: number }>());
+export const getCourseSuccess = createAction('[Courses] Get Course Success', props<{ course: Course }>());
+export const getCourseFailure = createAction('[Courses] Get Course Failure', props<{ errorMsg: string }>());
+
+export const searchCourses = createAction('[Courses] Search Courses', props<{ newValue: string }>());
+export const searchCoursesSuccess = createAction('[Courses] Search Courses Success', props<{ courses: Course[] }>());
+export const searchCoursesFailure = createAction('[Courses] Search Courses Failure', props<{ errorMsg: string }>());
+
+export const sortCourses = createAction('[Courses] Sort Courses', props<{ value: string }>());
+export const sortCoursesSuccess = createAction('[Courses] Sort Courses Success', props<{ courses: Course[] }>());
+export const sortCoursesFailure = createAction('[Courses] Sort Courses Failure', props<{ errorMsg: string }>());
+
+export const loadMoreCourses = createAction('[Courses] Load More Courses', props<{ amount: number }>());
+export const loadMoreCoursesSuccess = createAction(
+  '[Courses] Load More Courses Success',
+  props<{ courses: Course[] }>()
+);
+export const loadMoreCoursesFailure = createAction(
+  '[Courses] Load More Courses Failure',
+  props<{ errorMsg: string }>()
+);
 
 export const createCourse = createAction('[Courses] Create Course', props<{ course: Course }>());
 export const createCourseSuccess = createAction('[Courses] Create Course Success', props<{ course: Course }>());
