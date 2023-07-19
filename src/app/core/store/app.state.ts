d95cb004062fa9e6b47ffeab2ddd015919568dd6
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-19 20:36:07 +0300

    Update app.state.ts

--- src/app/core/store/app.state.ts
+++ src/app/core/store/app.state.ts
@@ -1,7 +1,14 @@
 import { AuthState } from './authentication/auth.reducer';
 import { CoursesState } from './courses/courses.reducer';
+import { authReducer } from './authentication/auth.reducer';
+import { coursesReducer } from './courses/courses.reducer';
 
 export interface AppState {
   auth: AuthState;
   courses: CoursesState;
 }
+
+export const reducers = {
+  auth: authReducer,
+  course: coursesReducer,
+};
