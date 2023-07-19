77a95cc6251e3a1b3c368fd860f3108762319c50
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-19 22:29:06 +0300

    Update add-course.component.ts

--- src/app/modules/course-form/add-course/add-course.component.ts
+++ src/app/modules/course-form/add-course/add-course.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { Store } from '@ngrx/store';
+
 import { Course } from '@shared/interfaces/course.interface';
+
+import { AppState } from '@store/app.state';
+import * as CoursesActions from '@store/courses/courses.actions';
 
 import { CoursesService } from '@services/courses/courses.service';
 
@@ -24,7 +29,12 @@ export class AddCourseComponent implements OnInit {
     isTopRated: false,
   };
 
-  constructor(private route: ActivatedRoute, private router: Router, private coursesService: CoursesService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private coursesService: CoursesService,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
@@ -41,9 +51,9 @@ export class AddCourseComponent implements OnInit {
     this.course.authors = this.author ? [{ id: 1, name: this.author }] : [];
 
     if (this.id && this.id !== 'new') {
-      this.coursesService.updateCourse(+this.id, this.course).subscribe(() => this.router.navigate(['/courses']));
+      this.store.dispatch(CoursesActions.updateCourse({ id: +this.id, course: this.course }));
     } else {
-      this.coursesService.createCourse(this.course).subscribe(() => this.router.navigate(['/courses']));
+      this.store.dispatch(CoursesActions.createCourse({ course: this.course }));
     }
   }
 
