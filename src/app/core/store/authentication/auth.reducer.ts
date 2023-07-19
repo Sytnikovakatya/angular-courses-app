e1a99cd3148ca50ca3aea289a1f8b0ebde59aa95
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-19 08:30:12 +0300

    Update auth.reducer.ts

--- src/app/core/store/authentication/auth.reducer.ts
+++ src/app/core/store/authentication/auth.reducer.ts
@@ -9,17 +9,25 @@ export interface AuthState {
   token: Token | null;
   user: User | null;
   isAuthenticated: boolean;
+  error: string | null;
 }
 
 export const initialAuthState: AuthState = {
   token: null,
   user: null,
   isAuthenticated: false,
+  error: null,
 };
 
 export const authReducer = createReducer(
   initialAuthState,
-  on(AuthActions.loginSuccess, (state, { token }) => ({ ...state, token })),
-  on(AuthActions.getUserInfo, (state, { user }) => ({ ...state, user, isAuthenticated: true })),
-  on(AuthActions.logout, state => ({ ...state, token: null, user: null, isAuthenticated: false }))
+  on(AuthActions.login, state => ({ ...state, error: null })),
+  on(AuthActions.loginSuccess, (state, { token }) => ({ ...state, token, error: null })),
+  on(AuthActions.loginFailure, (state, { errorMsg }) => ({ ...state, error: errorMsg })),
+
+  on(AuthActions.getUserInfo, state => ({ ...state, error: null })),
+  on(AuthActions.getUserInfoSuccess, (state, { user }) => ({ ...state, user, isAuthenticated: true, error: null })),
+  on(AuthActions.getUserInfoFailure, (state, { errorMsg }) => ({ ...state, error: errorMsg })),
+
+  on(AuthActions.logout, state => ({ ...state, token: null, user: null, isAuthenticated: false, error: null }))
 );
