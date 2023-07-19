22a0d55ec451885c0ed42c6bb5a8225931fe1b99
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-19 22:26:12 +0300

    Update courses.reducer.ts

--- src/app/core/store/courses/courses.reducer.ts
+++ src/app/core/store/courses/courses.reducer.ts
@@ -4,10 +4,12 @@ import * as CoursesActions from './courses.actions';
 
 export interface CoursesState {
   courses: Course[];
+  error: string | null;
 }
 
 const initialState: CoursesState = {
   courses: [],
+  error: null,
 };
 
 export const coursesReducer = createReducer(
@@ -16,14 +18,24 @@ export const coursesReducer = createReducer(
   on(CoursesActions.searchCourses, state => ({ ...state })),
   on(CoursesActions.sortCourses, (state, { courses }) => ({ ...state, courses })),
   on(CoursesActions.loadMoreCourses, (state, { courses }) => ({ ...state, courses })),
-  on(CoursesActions.createCourse, (state, { course }) => ({ ...state, courses: [...state.courses, course] })),
-  on(CoursesActions.updateCourse, (state, { id, course }) => ({
+
+  on(CoursesActions.createCourse, state => ({ ...state, error: null })),
+  on(CoursesActions.createCourseSuccess, (state, { course }) => ({ ...state, courses: [...state.courses, course] })),
+  on(CoursesActions.createCourseFailure, (state, { errorMsg }) => ({ ...state, error: errorMsg })),
+
+  on(CoursesActions.updateCourse, state => ({ ...state, error: null })),
+  on(CoursesActions.updateCourseSuccess, (state, { id, course }) => ({
     ...state,
-    courses: [...state.courses.filter(course => course.id !== id), course],
+    courses: state.courses.map(el => (el.id === id ? course : el)),
   })),
-  on(CoursesActions.removeCourse, (state, { id }) => ({
+  on(CoursesActions.updateCourseFailure, (state, { errorMsg }) => ({ ...state, error: errorMsg })),
+
+  on(CoursesActions.removeCourse, state => ({ ...state, error: null })),
+  on(CoursesActions.removeCourseSuccess, (state, { id }) => ({
     ...state,
     courses: state.courses.filter(course => course.id !== id),
   })),
+  on(CoursesActions.removeCourseFailure, (state, { errorMsg }) => ({ ...state, error: errorMsg })),
+
   on(CoursesActions.resetCourses, state => ({ ...state, courses: [] }))
 );
