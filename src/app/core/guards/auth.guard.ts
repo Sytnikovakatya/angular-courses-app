e1465a358f2004dad8397a8ce5158b6ed46c960d
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-22 22:02:23 +0300

    Update auth.guard.ts
     return Observable from this guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,17 +1,24 @@
 import { inject } from '@angular/core';
 import { Router, UrlTree } from '@angular/router';
 
+import { Observable, catchError, map, of } from 'rxjs';
+
 import { AuthService } from '@services/authentication/auth.service';
 
-export function authGuard(): boolean | UrlTree {
-  let authenticated = false;
+export function authGuard(): Observable<boolean | UrlTree> {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  authService.isAuthenticated.subscribe(auth => (authenticated = auth));
-
-  if (authenticated) {
-    return true;
-  }
-  return router.parseUrl('/login');
+  return authService.isAuthenticated.pipe(
+    map(authenticated => {
+      if (authenticated) {
+        return true;
+      }
+      return router.parseUrl('/login');
+    }),
+    catchError(() => {
+      router.parseUrl('/login');
+      return of(false);
+    })
+  );
 }
