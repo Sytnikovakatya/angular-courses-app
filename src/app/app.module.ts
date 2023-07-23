68929dd8d1120c97a0e153f558730b50725ef08b
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-24 00:11:37 +0300

    import the ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, isDevMode } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
@@ -39,6 +39,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     NgbModule,
     FormsModule,
+    ReactiveFormsModule,
     BreadcrumbModule,
     CoreModule,
     SharedModule,
--- src/app/modules/course-form/course-form.module.ts
+++ src/app/modules/course-form/course-form.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from '@shared/shared.module';
 
@@ -13,7 +13,7 @@ import { DateInputComponent } from './date-input/date-input.component';
 
 @NgModule({
   declarations: [AddCourseComponent, AuthorsComponent, DurationComponent, DateInputComponent],
-  imports: [CommonModule, SharedModule, FormsModule, ReactiveFormsModule, CourseFormRoutingModule],
+  imports: [CommonModule, SharedModule, FormsModule, CourseFormRoutingModule],
   exports: [AddCourseComponent],
 })
 export class CourseFormModule {}
