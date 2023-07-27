139fba59bf9d47558ffd5b58db8619446f8ae33c
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2023-07-27 23:58:14 +0300

    Update login.component.spec.ts

--- src/app/modules/login/login/login.component.spec.ts
+++ src/app/modules/login/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DefaultValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 
 import { StoreModule } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
@@ -11,11 +12,19 @@ import { LoginComponent } from './login.component';
 @Component({
   selector: 'app-input',
   template: '<div>Mock Input Component</div>',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: MockInputComponent,
+      multi: true,
+    },
+  ],
 })
-class MockInputComponent {
+class MockInputComponent extends DefaultValueAccessor {
   @Input() type: string;
   @Input() placeholder: string;
   @Input() bindModelData: string;
+  @Input() valid: boolean | undefined;
 }
 
 @Component({
@@ -26,6 +35,7 @@ class MockButtonComponent {
   @Input() text: string;
   @Input() type: string;
   @Input() class: string;
+  @Input() disabled: boolean;
 }
 
 describe('LoginComponent', () => {
@@ -34,7 +44,7 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, StoreModule.forRoot(provideMockStore)],
+      imports: [HttpClientTestingModule, ReactiveFormsModule, StoreModule.forRoot(provideMockStore)],
       declarations: [LoginComponent, MockInputComponent, MockButtonComponent],
     });
     fixture = TestBed.createComponent(LoginComponent);
